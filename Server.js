2773ecdf7fa6d2b7177b2a6dd9ad6c22e3c13fcd
Сергей <sergy7@mail.ru>
2015-08-03 20:33:21 +0000

    Парсинг описания группы + рефакторинг записи в бд

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var http = require('http');
+var VK = require('vksdk');
 var app = express();
 var server = http.createServer(app);
 var port = process.env.PORT || 3000;
@@ -16,36 +17,49 @@ var WebSocketServer = require("ws").Server;
 var wss = new WebSocketServer({server: server});
 
 var mongo = require('mongoskin');
-var conn = mongo.db("mongodb://admin:admin@ds035240.mongolab.com:35240/bigdata");
+var connectionBigData = mongo.db("mongodb://admin:admin@ds035240.mongolab.com:35240/bigdata");
+var connectionClientData = mongo.db("mongodb://admin:admin@ds043991.mongolab.com:43991/clientdata");
 
-function AddPostInBigDataDB(db,collection,post) {
-   this.db = db;
-   this.collection = collection;
-   this.post = post;
-   this.EntryInDB = function() {
-      conn.collection(collection).update(
-         { post: post.post },
-         { post: post.post, comment: post.comment, like: post.like },
-         { upsert:true },
-         function (error, count, status) {
-            console.log(error);
-            console.log(count);
-            console.log(status);
-         }
-      );
+var vk = new VK({
+   'appId'     : 5019588,
+   'appSecret' : 'dhk6znlyjSVdA3o4DTBt',
+   'language'  : 'ru'
+});
+
+vk.setSecureRequests(false);
+vk.request('groups.getById', {'group_id': "khl", 'fields': 'description'}, 'event');
+vk.on('event', function(data) {
+   
+   var VKPost = {
+      id: data.response[0].id,
+   	name: data.response[0].name,
+   	description: data.response[0].description
    };
-}
+   
+   function AddDataInBD(db,collection,data) {
+      this.db = db;
+      this.collection = collection;
+      this.EntryInDB = function() {
+         db.collection(collection).update(
+            { id: data.id }, 
+            data,
+            { upsert:true },
+            function (error, count, status) {
+               console.log(error);
+               console.log(count);
+               console.log(status);
+            }
+         );
+      };
+   }
 
-var VKPost = {
-   post: 10,
-   comment: 2,
-   like: 12
-};
+   var collection = 'wall';
+   
+   var RecordableVKPostInBD = new AddDataInBD(connectionBigData, collection, VKPost);
+   RecordableVKPostInBD.EntryInDB();
 
-var collection = 'wall';
+});
 
-var RecordableVKPostInBD = new AddPostInBigDataDB(conn, collection, VKPost);
-RecordableVKPostInBD.EntryInDB();
 
 wss.on("connection", function(ws) {
   console.log("websocket connection open");
@@ -61,78 +75,3 @@ wss.on("connection", function(ws) {
     console.log(msg);
   };
 });
-
-/*wss.on("connection", function(ws) {
-   console.log("websocket connection open");
-  
-   var mongo = require('mongoskin');
-   var conn = mongo.db("mongodb://admin:admin@ds041032.mongolab.com:41032/gamevk");
-   var id, summary;
-   
-      var msg = {
-         type: "news",
-         data: "hello world"
-      };
-      
-      ws.send(JSON.stringify(msg));
-    
-      ws.onmessage = function(event) {
-         
-         var type = JSON.parse(event.data).type;
-         
-         switch (type) {
-            case "result":
-               event = JSON.parse(event.data);
-               id = event.id;
-               summary = event.summary;
-               console.log("id " + id);
-               console.log("summary " + summary);
-               conn.collection('players').update(
-                  { id: id },
-                  { id: id, summary: summary },
-                  { upsert:true },
-                  function (err, count, status) {
-                     console.log(err);
-                     console.log(count);
-                     console.log(status);
-                  }
-               );
-               break;
-            case "init":
-               var idU= JSON.parse(event.data).id;
-               console.log("idU " + idU);
-               conn.collection('players').findOne(
-               {
-                  id: idU
-               },
-               function(err, doc)
-               {
-                  if (err) {   }
-                  if (doc) { 
-                     var summaryUser = {
-                        summary: doc.summary
-                     }; 
-                     console.log("summaryUser " + summaryUser.summary);
-                     ws.send(JSON.stringify(summaryUser));
-                  }
-                  if (!doc) {
-                     conn.collection('players').update(
-                        { id: idU },
-                        { id: idU, summary: "0" },
-                        { upsert:true },
-                        function (err, count, status) {
-                           console.log(err);
-                           console.log(count);
-                           console.log(status);
-                        }
-                     );
-                     summaryUser = {summary: "0" };
-                     ws.send(JSON.stringify(summaryUser));
-                  }
-               });
-               break;
-            default:
-               break;
-         }
-      };
-});*/
